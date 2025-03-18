639bc8e56fca9d9e18b8438a73df10947b593aff
Joahan3250 <joahan.sanchez@alogasoftwaresolution.com>
2025-03-18 09:55:56 -0600

    Primeros cambios

---
+++ OneDrive/Escritorio/react/react-app/src/Titulo.tsx
@@ -0,0 +1,10 @@
+function Titulo() {
+
+    const nombre = "Joahan";
+    if(nombre){
+      return <p>Hola {nombre}</p>
+    }
+    return <p>Hola mundo</p>;
+    }
+    
+    export default Titulo;
