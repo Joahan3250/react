19afa918642eef68021978a7a4aaea5195641512
Joahan3250 <joahan.sanchez@alogasoftwaresolution.com>
2025-03-18 11:23:33 -0600

    imprimiendo mi primera lista

--- OneDrive/Escritorio/react/react-app/src/App.tsx
+++ OneDrive/Escritorio/react/react-app/src/App.tsx
@@ -1,8 +1,11 @@
 import Card, { CardBody } from './components/Card';
+import List from './components/List';
 function App() {
+    const list = ["Goku","Vegeta", "Raditz"]
     return (
         <Card>
             <CardBody title='Hola mundo' text='Este es el texto' />
+            <List data={list}/>
         </Card>
     );
 }
